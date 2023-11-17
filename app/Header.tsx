b349866cf54d9e12fd104149cfc6f3711e79a063
shirko <shir617@gmail.com>
2023-11-16 21:09:21 -0500

    Nav Links added and styled

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,22 +1,31 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import React from "react";
+import NavLinks from "./NavLinks";
 
 function Header() {
   return (
     <header>
       <div className="grid grid-cols-3 p-10 items-center">
-        <Bars3Icon className="h-8 w-8 cursor-pointer"/>
-        <Link href='/' prefetch={false}>
-            <h1>NewsFlowHub</h1>
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+        <Link href="/" prefetch={false}>
+          <h1 className="font-serif text-4xl text-center">
+            <span className="underline decoration-6 decoration-orange-400">
+              NewsFlowHub
+            </span>
+          </h1>
         </Link>
-        <div>
-            {/* Darkmode*/}
-            <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
-                Subscribe Now
-            </button>
+        <div className="flex items-center justify-end space-x-2">
+          {/* Darkmode*/}
+          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
+            Subscribe Now
+          </button>
         </div>
       </div>
+
+      <NavLinks />
+
+      {/* SearchBox */}
     </header>
   );
 }
