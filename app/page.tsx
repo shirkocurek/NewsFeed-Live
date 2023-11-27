963bc9c9fa669c22a07bef61a6d328b0bdc2592b
shirko <shir617@gmail.com>
2023-11-27 11:49:45 -0500

    data source updated to media stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { categories } from "@/constants";
 import fetchNews from "@/lib/fetchNews";
 import NewsList from "./NewsList";
-import response from '../response.json'
+import response from "../response.json";
 import { resolve } from "path";
 
 async function Homepage() {
   //fetch the news data
-  const news: NewsResponse = response || await fetchNews(categories.join(","));
+  const news: NewsResponse = await fetchNews(categories.join(","));
   // set timeout for 3 seconds
-// await new Promise((resolve) => setTimeout(resolve,3000))
+  // await new Promise((resolve) => setTimeout(resolve,3000))
 
   return (
     <div>
